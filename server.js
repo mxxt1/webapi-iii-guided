950cf2018c91f5606d1c4d1aaa9351b87fb1e043
mxxt1 <matt@dustcloudlabs.com>
2019-10-30 13:29:31 -0400

    gateKeeper updated

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
 const hubsRouter = require('./hubs/hubs-router.js');
+const morgan = require('morgan');
 
 const server = express();
 
-function dateLogger(req, res, next) {
+// function dateLogger(req, res, next) {
 
-  console.log(new Date().toISOString());
-  next();
-};
+//   console.log(new Date().toISOString());
+//   next();
+// };
 
 function httpMethod(req,res,next){
+  console.log(new Date().toISOString());
   console.log(req.method);
   console.log(req.get('host'));
   console.log(req.originalUrl);
-
   next();
+}
+
+function gateKeeper(req,res,next){
+  //new way of reading data from the client
+  const password= req.headers.password;
+
+    if (password.toLowerCase() === 'mellon'){
+      next();
+    } else if (!password.toLowerCase()){
+      res.status(400).json({message: 'Please enter a password'});
+    }else {
+      res.status(401).json({message: 'You cannot pass!'});
+    }
 }
 
 
@@ -24,11 +38,12 @@ server.use(express.json());
 // can also use:
 // server.use(helmet(), express.json())
 
-
-
+server.use(gateKeeper);
+server.use(morgan('dev'));
 server.use(httpMethod);
-server.use(dateLogger);
+// server.use(dateLogger);
 server.use('/api/hubs', hubsRouter);
+
 
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
