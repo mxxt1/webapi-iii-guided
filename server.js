cd1297b2f333e43117c0d568106927a1a87fc858
mxxt1 <matt@dustcloudlabs.com>
2019-10-30 13:03:40 -0400

    custom hook for http method

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express'); // importing a CommonJS module
-
+const helmet = require('helmet');
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+function dateLogger(req, res, next) {
 
+  console.log(new Date().toISOString());
+  next();
+};
+
+function httpMethod(req,res,next){
+  console.log(req.method);
+  console.log(req.get('host'));
+  console.log(req.originalUrl);
+
+  next();
+}
+
+
+server.use(helmet());
+server.use(express.json());
+// can also use:
+// server.use(helmet(), express.json())
+
+
+
+server.use(httpMethod);
+server.use(dateLogger);
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
